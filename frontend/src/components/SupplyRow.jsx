bf0ff025928668efbc9391ed67c51dbcc813fe6e
Jeff <jeff@jeffreysparrow.net>
2025-12-08 21:41:00 -0500

    citation comment

--- frontend/src/components/SupplyRow.jsx
+++ frontend/src/components/SupplyRow.jsx
@@ -1,12 +1,18 @@
+// Citation:
+// Date: 12/08/2025
+// Adapted from: CS340 Exporations/Activities
+// Source URL: https://canvas.oregonstate.edu/courses/2017561/pages/exploration-web-application-technology-2
 
 import React from 'react';
 import DeleteSupplyForm from './DeleteSupplyForm';
 
 const SupplyRow = ({ rowObject, backendURL, refreshData }) => (
-    <tr>
+    <tr>e
         {Object.values(rowObject).map((v, idx) => <td key={idx}>{v}</td>)}
         <td>
-            <DeleteSupplyForm rowObject={rowObject} backendURL={backendURL} refreshData={refreshData} />
+            <DeleteSupplyForm rowObject={rowObject} 
+                              backendURL={backendURL} 
+                              refreshData={refreshData} />
         </td>
     </tr>
 );
